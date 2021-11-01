9dbd2fe63988a7b71d57235721912f475cd01dad
Giancarlos Salas <giansalex@gmail.com>
2021-11-01 15:33:37 -0500

    Deploy wasm v1

---
+++ precache-manifest.8134885ead2fe08aa7c1d79d70ae1193.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3701c23df5c3b003cd6d9a08397a3db5",
+    "url": "https://blueprints.juno.giansalex.dev//index.html"
+  },
+  {
+    "revision": "53d3d6369173f794d2b5",
+    "url": "https://blueprints.juno.giansalex.dev//static/css/2.8ba42a9a.chunk.css"
+  },
+  {
+    "revision": "dc4b5e2bee6d703eae89",
+    "url": "https://blueprints.juno.giansalex.dev//static/css/main.e96feb9f.chunk.css"
+  },
+  {
+    "revision": "53d3d6369173f794d2b5",
+    "url": "https://blueprints.juno.giansalex.dev//static/js/2.6b0f74b2.chunk.js"
+  },
+  {
+    "revision": "bb4e22aa112d81684820c9174b0b5154",
+    "url": "https://blueprints.juno.giansalex.dev//static/js/2.6b0f74b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc4b5e2bee6d703eae89",
+    "url": "https://blueprints.juno.giansalex.dev//static/js/main.00d1d71a.chunk.js"
+  },
+  {
+    "revision": "2a882e8cf9dfcf5a8982",
+    "url": "https://blueprints.juno.giansalex.dev//static/js/runtime-main.d2d4258c.js"
+  },
+  {
+    "revision": "7435b81a6effef8b2f866fb6f98c687a",
+    "url": "https://blueprints.juno.giansalex.dev//static/media/background.7435b81a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://blueprints.juno.giansalex.dev/precache-manifest.d261f8dc29b7e4c5f96d49679ac98950.js"
+  "https://blueprints.juno.giansalex.dev//precache-manifest.8134885ead2fe08aa7c1d79d70ae1193.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://blueprints.juno.giansalex.dev/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://blueprints.juno.giansalex.dev//index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
