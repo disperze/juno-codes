c1b671d39a81ceee739dc78aa9542dce63a0768f
Giancarlos Salas <giansalex@gmail.com>
2021-12-02 14:26:05 -0500

    Add account link

--- src/pages/contract/ContractTable.tsx
+++ src/pages/contract/ContractTable.tsx
@@ -1,15 +1,15 @@
 import React from "react"
 import { ContractLink } from "../../components/ContractLink"
-import {  ContractElement } from "../../types/contract"
-import { ellideMiddle, printableBalance } from "../../ui-utils"
+import { ContractElement } from "../../types/contract"
+import { printableBalance } from "../../ui-utils"
 import { settings } from "../../settings";
+import { AccountLink } from "../../components/AccountLink";
 
 interface Props {
     readonly contracts: ContractElement[],
-    readonly maxLength?: number | null;
 }
 
-export default function ContractTable({ contracts, maxLength = 20 }: Props): JSX.Element {
+export default function ContractTable({ contracts }: Props): JSX.Element {
 
     return (
         <table className="table">
@@ -28,8 +28,8 @@ export default function ContractTable({ contracts, maxLength = 20 }: Props): JSX
                     <tr key={address}>
                         <td>{label}</td>
 
-                        <td><ContractLink address={address} /> </td>
-                        <td>{ellideMiddle(creator, maxLength || 99999)}</td>
+                        <td><ContractLink address={address} maxLength={25} /> </td>
+                        <td><AccountLink address={creator} maxLength={20} /></td>
                         <td>{tx}</td>
                         <td>{gas}</td>
                         <td>{printableBalance([{amount: fees.toString(), denom: settings.backend.denominations[0]}])}</td>
