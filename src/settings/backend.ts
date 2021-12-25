00aedf5947003439a0e29c4b89b81f2ef107d198
Giancarlos Salas <giansalex@gmail.com>
2021-12-24 20:11:46 -0500

    Add mainnet config

--- src/settings/backend.ts
+++ src/settings/backend.ts
@@ -110,8 +110,55 @@ const uniSettings: BackendSettings = {
         coinDecimals: 6,
       },
     ],
-    features: ["stargate", "ibc-transfer", "cosmwasm", "no-legacy-stdTx"],
+    features: ["stargate", "ibc-transfer", "cosmwasm", "no-legacy-stdTx", "ibc-go"],
     explorerUrlToTx: "https://uni.junoscan.com/transactions/{txHash}",
+
+  },
+  contractsUrl: "https://graph.juno.giansalex.dev/api/rest/page"
+};
+
+const juno1Settings: BackendSettings = {
+  nodeUrls: ["https://rpc-juno.itastakers.com"],
+  denominations: ["ujuno"],
+  addressPrefix: "juno",
+  gasPrice: GasPrice.fromString("0.025ujuno"),
+  keplrChainInfo: {
+    rpc: "https://rpc-juno.itastakers.com:443",
+    rest: "https://lcd-juno.itastakers.com:443",
+    chainId: "juno-1",
+    chainName: "Juno",
+    stakeCurrency: {
+      coinDenom: "JUNO",
+      coinMinimalDenom: "ujuno",
+      coinDecimals: 6,
+    },
+    bip44: {
+      coinType: 118,
+    },
+    bech32Config: {
+      bech32PrefixAccAddr: "juno",
+      bech32PrefixAccPub: "junopub",
+      bech32PrefixValAddr: "junovaloper",
+      bech32PrefixValPub: "junovaloperpub",
+      bech32PrefixConsAddr: "junovalcons",
+      bech32PrefixConsPub: "junovalconspub",
+    },
+    currencies: [
+      {
+        coinDenom: "JUNO",
+        coinMinimalDenom: "ujuno",
+        coinDecimals: 6,
+      },
+    ],
+    feeCurrencies: [
+      {
+        coinDenom: "JUNO",
+        coinMinimalDenom: "ujuno",
+        coinDecimals: 6,
+      },
+    ],
+    features: ["stargate", "ibc-transfer", "cosmwasm", "no-legacy-stdTx", "ibc-go"],
+    explorerUrlToTx: "https://mintscan.io/juno/txs/{txHash}",
 
   },
   contractsUrl: "https://graph.juno.giansalex.dev/api/rest/page"
@@ -122,6 +169,7 @@ const knownBackends: Partial<Record<string, BackendSettings>> = {
   musselnet: musselnetSettings,
   pebblenet: pebblenetSettings,
   uninet: uniSettings,
+  juno1: juno1Settings,
 };
 
 export function getCurrentBackend(): BackendSettings {
