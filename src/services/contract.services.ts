649470998dbf7e5d04d664576e5b044bf5268111
Giancarlos Salas <giansalex@gmail.com>
2022-01-25 17:04:37 -0500

    Update domain api url

--- src/services/contract.services.ts
+++ src/services/contract.services.ts
@@ -2,17 +2,17 @@ import { settings } from "../settings";
 
 export default class ContractService {
   getContracts = async (pageSize: number = 10, offset: number = 0) => {
-      const response = await fetch(`${settings.backend?.contractsUrl}/contracts/${pageSize}/${offset}`);
+      const response = await fetch(`${settings.backend?.contractsUrl}contracts/${pageSize}/${offset}`);
       const contracts = await response.json()
       return contracts;
   }
   getTokens = async (pageSize: number = 10, offset: number = 0) => {
-    const response = await fetch(`${settings.backend?.contractsUrl}/tokens/${pageSize}/${offset}`);
+    const response = await fetch(`${settings.backend?.contractsUrl}tokens/${pageSize}/${offset}`);
     const contracts = await response.json()
     return contracts;
   }
   getCodes = async (pageSize: number = 10, offset: number = 0) => {
-    const codes = await fetch(`${settings.backend?.contractsUrl}/codes/${pageSize}/${offset}`)
+    const codes = await fetch(`${settings.backend?.contractsUrl}codes/${pageSize}/${offset}`)
                             .then(res => res.json());
     return codes;
   }
