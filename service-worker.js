a9ce466a544cb8c31bbe270ef9e706a398ca3f08
giansalex <giansalex@gmail.com>
2023-04-11 04:13:42 +0000

    Deploying to gh-pages from @ disperze/juno-codes@934583c6fc1f4402284c8bf8ad9f296c417bc446 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.773755415a5bfea4736cdb5716be07a2.js"
+  "/precache-manifest.47018a83ab5daf2cec0688383ff8507c.js"
 );
 
 self.addEventListener('message', (event) => {
